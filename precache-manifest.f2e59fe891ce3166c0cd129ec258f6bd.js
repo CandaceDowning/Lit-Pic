8b62717b424c7daf9169b651762f88235f481359
Candace <candacedowning@gmail.com>
2019-04-12 18:01:56 -0500

    Updates

---
+++ precache-manifest.f2e59fe891ce3166c0cd129ec258f6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cba3ad1cc9a7d8db2dd7",
+    "url": "/lit-pic/static/js/runtime~main.9dc22a44.js"
+  },
+  {
+    "revision": "1e21f1ef75a67f66e7f9",
+    "url": "/lit-pic/static/js/main.c5177234.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/lit-pic/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1e21f1ef75a67f66e7f9",
+    "url": "/lit-pic/static/css/main.d7d4044b.chunk.css"
+  },
+  {
+    "revision": "e8b2ea9cb5a69e31084b6aa2a7a7300c",
+    "url": "/lit-pic/index.html"
+  }
+];
